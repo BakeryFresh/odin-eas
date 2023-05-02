3f8778e51989527cd9d0f0bd58dfe82568bdeedd
BakeryFresh <shamanmclean@gmail.com>
2023-05-02 16:06:16 +0200

    Add function to clear grid

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 gridSize = document.getElementById('gridSize');
 grid = document.getElementById('grid');
 
-console.log(gridSize.value);
-console.log(grid);
 
+//calls func to generate grid based on slider size
 btnOne = document.getElementById('btnOne');
 btnOne.addEventListener('click', function() {
+    removeChildNodes(grid);
     generateGrid(gridSize.value);
 });
 
-
+//generate grid size and sort grid row/column value based on user input. Squares the user choice to create a grid
 function generateGrid(size = 4) {
-    // grid.style.cssText = 'grid-template-columns: repeat(`${size}`, 1fr)';
     grid.setAttribute('style', `grid-template-columns: repeat(${size}, 2fr); grid-template-rows: repeat(${size}, 2fr);`);
     console.log("function started");
     size = size*size;
@@ -24,6 +23,12 @@ function generateGrid(size = 4) {
 
         grid.appendChild(cell);
         console.log("Div should be added");
-        
+    }
+}
+
+//clear the grid of spaces to reset for another grid generation
+function removeChildNodes(parent) {
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
     }
 }
