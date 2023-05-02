558290fd563bd39fa236bd81977a1c4a2c543848
BakeryFresh <shamanmclean@gmail.com>
2023-05-02 14:24:51 +0200

    Update function to allow for custom grid sizes. Add background CSS for grid

--- script.js
+++ script.js
@@ -10,15 +10,17 @@ btnOne.addEventListener('click', function() {
 });
 
 
-function generateGrid(size = 2) {
-    grid.style.cssText = 'display: grid; '
+function generateGrid(size = 4) {
+    // grid.style.cssText = 'grid-template-columns: repeat(`${size}`, 1fr)';
+    grid.setAttribute('style', `grid-template-columns: repeat(${size}, 2fr); grid-template-rows: repeat(${size}, 2fr);`);
     console.log("function started");
-
+    size = size*size;
     for (let i = 0; i < size; i++) {
         const cell = document.createElement('div');
 
         cell.classList.add('Cell', "Cell-" + i)
         cell.textContent = "cell:" + i;
+        cell.classList.add('cell');
 
         grid.appendChild(cell);
         console.log("Div should be added");
